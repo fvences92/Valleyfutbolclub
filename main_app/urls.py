8a6d9b3cc9f6c9448e64ef7495dbdfa3484974a8
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-06-23 10:54:14 -0700

    static file working

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path ('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('players/', views.player_index, name ='index')
+    path('players/', views.players_index, name ='index')
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main_app/views.py
+++ main_app/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟﾉ</h1>')
+  return HttpResponse('<h1>Valley Futbol Club</h1>')
 
 def about(request):
   return render(request, 'about.html')
 
-def player_index(request): 
+def players_index(request): 
   return render (request, 'players/index.html' , {'players': players})
